566c5983367a60b2a59451235eeb4294a09fa534
Coby Chapple <coby@github.com>
2013-01-09 15:27:09 +0000

    add info block

--- jquery.caret.js
+++ jquery.caret.js
@@ -1,3 +1,10 @@
+/*
+ * jquery-caret
+ * https://github.com/cobyism/jquery-caret
+ * Copyright (c) 2012 Coby Chapple (coby@github.com)
+ * Released under the MIT license.
+ */
+
 (function($) {
   $.fn.caret = function(index){
     if (index){
